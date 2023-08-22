b1e36d94d4ca1c51116325df396fa3a9eeba7a2f
Momen Mahmoud <73801895+momen-mahmoud@users.noreply.github.com>
2023-08-22 17:17:14 +0300

    Update images.py

--- images.py
+++ images.py
@@ -5,11 +5,10 @@ import csv
 from datetime import datetime
 
 def extract_images(input_folder, output_folder, report_csv):
-    # Create necessary folders
     if not os.path.exists(output_folder):
         os.makedirs(output_folder)
 
-    # Initialize the CSV report
+   
     with open(report_csv, 'w', newline='') as csvfile:
         csv_writer = csv.writer(csvfile)
         csv_writer.writerow(['Image Name', 'Image Size', 'Last Modification Date'])
@@ -17,29 +16,29 @@ def extract_images(input_folder, output_folder, report_csv):
     prefix_to_discard = "prefix_to_discard"  # Replace with your actual prefix
     image_counter = 1  # Counter for renaming images
 
-    # Traverse the dataset directory
+  
     for root, _, files in os.walk(input_folder):
         for file in files:
             if file.lower().endswith(('.png', '.jpg', '.jpeg', '.gif')):
                 image_path = os.path.join(root, file)
 
-                # Extract image name without prefix
+             
                 image_name = file[len(prefix_to_discard):]  # Remove the prefix
                 new_image_name = f"image{image_counter}.jpg"  # New image name with counter
 
-                # Copy image to output folder with new name
+              
                 new_image_path = os.path.join(output_folder, new_image_name)
                 shutil.copy(image_path, new_image_path)
 
-                # Get image size and modification date
+           
                 image = Image.open(image_path)
                 image_size = os.path.getsize(new_image_path)
                 modification_date = os.path.getmtime(image_path)
 
-                # Convert modification date to MM/DD/YYYY format
+            
                 formatted_date = datetime.fromtimestamp(modification_date).strftime('%m/%d/%Y')
 
-                # Append information to the CSV report
+             
                 with open(report_csv, 'a', newline='') as csvfile:
                     csv_writer = csv.writer(csvfile)
                     csv_writer.writerow([new_image_name, image_size, formatted_date])
